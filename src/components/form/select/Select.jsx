a6e633922e2256d24be85cdd35b686ddc44893ac
jorgeprj <jlmp2002@gmail.com>
2023-09-23 20:00:47 -0300

    fix: required input and select

--- src/components/form/select/Select.jsx
+++ src/components/form/select/Select.jsx
@@ -4,7 +4,7 @@ const Select = ( { text, name, options, handleOnChange, value } ) => {
   return (
     <div className="form-select">
         <label htmlFor={name}>{text}</label>
-        <select name={name} id={name} onChange={handleOnChange} value={value || ""}>
+        <select name={name} id={name} onChange={handleOnChange} value={value || ""} required>
             <option>Select an option</option>
             {options.map((option) => (
               <option value={option.id} key={option.id} >{option.name}</option>
