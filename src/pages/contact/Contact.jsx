edd13f03e6984d89c56b776406b3c9470ba416e6
jorgeprj <jlmp2002@gmail.com>
2023-09-24 01:19:23 -0300

    feat: contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,23 @@
+import './Contact.css'
+
 const Contact = () => {
-  return (
-    <div className="main">
-      Contact
-    </div>
-  )
+
+	return (
+		<div className="main">
+			<div className='contact'>
+				<div className='info'>
+				<h1>Contact Me</h1>
+				<h2>Thank you for viewing the project</h2>
+				<h3>Please choose the best way to contact me</h3>
+				<ul>
+					<li>Github: <span><a href="https://github.com/jorgeprj">@jorgeprj</a></span></li>
+					<li>Email: <span><a href="jlmp2002@gmail.com">jlmp2002@gmail.com</a></span></li>
+					<li>Linkedin: <span><a href="https://www.linkedin.com/in/jorgeprj/">/jorgeprj</a></span></li>
+				</ul>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Contact
